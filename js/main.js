fa0b3d50daf43d848df0c75a0b91619b987a844d
Kyle Maharlika <kyle.maharlika@gmail.com>
2015-06-01 18:35:50 -0400

    Updated orbit circles to respond to mouseover

--- js/main.js
+++ js/main.js
@@ -7,17 +7,19 @@ function draw(geo_data) {
     var center_x = (width + margin)/2;
     var center_y = (height + margin)/2;
     var main_radius = 200;
+    var smaller_radius = 80;
     var default_duration = 300;
     var fade_duration = 500;
-    var font = 'Oswald';
-    var title_font_size = 30;
+    var font = 'Lato';
+    var orbital_font = 'Lato';
+    var title_font_size = 24;
 
     var svg = d3.select("body")
         .append("svg")
         .attr("width", width + margin)
         .attr("height", height + margin)
         .append('g')
-        .attr('class', 'map');
+        .attr('class', 'screen');
 
     var years = [];
 
@@ -28,8 +30,8 @@ function draw(geo_data) {
     }
 
     var titantic_title = [
-      'Who lived and who died',
-      'on the Titanic shipwreck?'
+      'WHO LIVED AND WHO DIED',
+      'ON THE TITANIC SHIPWRECK?'
     ];
 
     var main_navigation = svg.append('g')
@@ -52,6 +54,7 @@ function draw(geo_data) {
                    .attr('y', function(d, i) {
                      return center_y + i*title_font_size;
                    })
+                   .attr('class', 'title-text')
                    .attr('font-family', font)
                    .attr('font-size', title_font_size + 'px')
                    .attr('fill', 'black')
@@ -62,16 +65,6 @@ function draw(geo_data) {
       main_navigation
         .transition()
         .duration(default_duration)
-        .style('opacity', 1);
-      
-      svg.select('circle')
-         .transition()
-         .duration(default_duration)
-         .style('stroke-width', 3);
-
-      main_navigation
-        .transition()
-        .duration(default_duration)
         .style('opacity', 0)
         .remove();
 
@@ -79,6 +72,15 @@ function draw(geo_data) {
         .transition()
         .duration(default_duration)
         .attr('r', 0);
+
+      main_navigation.selectAll('text.title-text')
+        .transition()
+        .duration(default_duration)
+        .attr('font-size', 0)
+        .attr('x', center_x)
+        .attr('y', center_y);
+
+      explode();
     });
 
     window.setTimeout(function() {
@@ -89,10 +91,94 @@ function draw(geo_data) {
     }, fade_duration);
 
     function explode() {
-      var rotation_radius = 400;
+      var rotation_radius = 200;
       var num_circles = 3;
+      var category_text = ['BY CLASS?', 'BY SEX?', 'BY AGE?'];
       var circles = [];
+      var radians_in_circle = 2*Math.PI;
 
+      for (var i = 0; i < num_circles; i++) {
+        var new_group = svg.append('g').attr('class', 'orbit');
+        var theta = Math.PI/2 + i*(radians_in_circle/num_circles);
+        var cartesian_coordinates = polar_to_cartesian(rotation_radius, theta);
+        var current_text = category_text[i];
+        var new_x = center_x - cartesian_coordinates['x'];
+        var new_y = center_y - cartesian_coordinates['y'];
+
+        var circle = new_group.append('circle')
+          .attr('class', 'orbital-circle')
+          .attr('r', 0)
+          .attr('cx', center_x)
+          .attr('cy', center_y)
+          .transition()
+          .duration(1000)
+          .attr('r', smaller_radius)
+          .attr('cx', new_x)
+          .attr('cy', new_y);
+
+        new_group.append('text')
+          .attr('class', 'orbital-circle-text')
+          .attr('x', center_x)
+          .attr('y', center_y)
+          .attr('font-size', '0px')
+          .attr('fill', 'white')
+          .transition()
+          .duration(1000)
+          .attr('x', new_x)
+          .attr('y', new_y + title_font_size/2)
+          .attr('font-family', font)
+          .attr('font-size', title_font_size + 'px')
+          .attr('fill', 'black')
+          .style('text-anchor', 'middle')
+          .text(category_text[i]);
+      }
+
+      function setOrbitMouseEvents() {
+        svg.selectAll('g.orbit')
+          .on('mouseover', function() {
+            var orbit = d3.select(this);
+
+            orbit.select('circle.orbital-circle')
+              .transition()
+              .duration(default_duration)
+              .style('stroke-width', 3);
+
+            orbit.select('text.orbital-circle-text')
+              .transition()
+              .duration(default_duration)
+              .attr('fill', 'white');
+          });
+
+        svg.selectAll('g.orbit')
+          .on('mouseout', function() {
+            var orbit = d3.select(this);
+
+            orbit.select('circle.orbital-circle')
+              .transition()
+              .duration(default_duration)
+              .style('stroke-width', 1);
+
+            orbit.select('text.orbital-circle-text')
+              .transition()
+              .duration(default_duration)
+              .attr('fill', 'black');
+          });
+      }
+
+      window.setTimeout(setOrbitMouseEvents, 500);
+    }
+
+    function scatter_others(clicked_circle) {
+      // mark the selected circle with 'selected'
+    }
+
+    function polar_to_cartesian(r, theta) {
+      var cartesian_coordinates = [];
+      var x = r*Math.cos(theta);
+      var y = r*Math.sin(theta);
+      cartesian_coordinates['x'] = x;
+      cartesian_coordinates['y'] = y;
+      return cartesian_coordinates;
     }
   
 };
